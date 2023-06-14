42bc6c7294a6ff62e671e4e40969346e9c6c0c4d
cesaroer <cesarvarcard@gmail.com>
2023-06-13 23:13:36 -0600

    Added title and play btn

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -8,7 +8,38 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [CustomAppBar(), ImagenDiscoYDuracion()],
+        children: [CustomAppBar(), ImagenDiscoYDuracion(), TitlePlay()],
+      ),
+    );
+  }
+}
+
+class TitlePlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      margin: EdgeInsets.only(top: 40),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text("Far Away",
+                  style: TextStyle(
+                      fontSize: 30, color: Colors.white.withOpacity(0.8))),
+              Text("Breaking Benjamin",
+                  style: TextStyle(
+                      fontSize: 15, color: Colors.white.withOpacity(0.5))),
+            ],
+          ),
+          Spacer(),
+          FloatingActionButton(
+            elevation: 0,
+            onPressed: () {},
+            backgroundColor: Color(0xfff8cb51),
+            child: Icon(Icons.play_arrow),
+          )
+        ],
       ),
     );
   }
