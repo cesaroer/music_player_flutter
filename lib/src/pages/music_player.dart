869ce1a6733c990948d9f487b3f9759f4db56cee
cesaroer <cesarvarcard@gmail.com>
2023-06-13 23:08:03 -0600

    Added progress bar

--- lib/src/pages/music_player.dart
+++ lib/src/pages/music_player.dart
@@ -18,13 +18,51 @@ class ImagenDiscoYDuracion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      margin: EdgeInsets.only(top: 70),
       child: Row(
         children: [
-          ImagenDisco()
-
-          //Barra progreso
+          ImagenDisco(),
+          SizedBox(width: 20),
+          BarraProgreso(),
+          SizedBox(width: 20)
 
           //
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgreso extends StatelessWidget {
+  final style = TextStyle(color: Colors.white.withOpacity(0.4));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("0:00", style: style),
+          SizedBox(height: 10),
+          Stack(
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.1),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 3,
+                  height: 100,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text("0:00", style: style),
         ],
       ),
     );
@@ -35,36 +73,37 @@ class ImagenDisco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
       width: 250,
       height: 250,
       child: ClipRRect(
-          borderRadius: BorderRadius.circular(200),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Image(image: AssetImage('assets/aurora.jpg')),
-              Container(
-                width: 25,
-                height: 25,
-                decoration: BoxDecoration(
-                  color: Colors.black38,
-                  borderRadius: BorderRadius.circular(100),
-                ),
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image(image: AssetImage('assets/aurora.jpg')),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(100),
               ),
-              Container(
-                width: 18,
-                height: 18,
-                decoration: BoxDecoration(
-                  color: Color(0xff1c1c25),
-                  borderRadius: BorderRadius.circular(100),
-                ),
+            ),
+            Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                color: Color(0xff1c1c25),
+                borderRadius: BorderRadius.circular(100),
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(200),
-        gradient: LinearGradient(
+        gradient: const LinearGradient(
           begin: Alignment.topLeft,
           colors: [Color(0xff484750), Color(0xff1e1c24)],
         ),
